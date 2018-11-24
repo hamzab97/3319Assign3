e4dd340854473992c5709a7b4b23e4f0812007fc
hamzab97 <hbana3@uwo.ca>
2018-11-24 14:35:37 -0500

    commit

--- getUserData.php
+++ getUserData.php
@@ -1,13 +1,13 @@
 <?php
 //get customer data from mysql db
-    $query = "SELECT FirstName, LastName FROM Customer ORDER BY Customer.LastName"; //get all the data from customer table, order it by the lastname
+    $query = "SELECT FirstName, LastName, CustomerID FROM Customer ORDER BY Customer.LastName"; //get all the data from customer table, order it by the lastname
     $result = mysqli_query($connection,$query);
     if (!$result) {
          die("databases query failed.");
     }
     while ($row = mysqli_fetch_assoc($result)) {
-        echo '<a class="list-group-item list-group-item-action" data-toggle="list" role="tab" href="#$row[FirstName]">';
-        echo $row["FirstName"];
+        echo '<a class="list-group-item list-group-item-action" data-toggle="list" role="tab" href="#$row[FirstName]" value="$row["CustomerID"]">';
+        echo $row["FirstName"] " " $row["LastName"];
         echo '</a>';
 //        var_dump($row);
 //        echo $row;
