0f7a13999c744cb7543bd1eb921d4c703c5835f1
hamzab97 <hbana3@uwo.ca>
2018-11-24 12:39:50 -0500

    commit

--- getUserData.php
+++ getUserData.php
@@ -6,7 +6,7 @@
          die("databases query failed.");
     }
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<a>";
+        echo "<a class=list-group-item list-group-item-action>";
         echo $row["FirstName"];
         echo "</a>";
 //        var_dump($row);
