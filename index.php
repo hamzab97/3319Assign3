f89807958d5ea8b9042fae06bf13f177b2f0616b
hamzab97 <hbana3@uwo.ca>
2018-11-24 10:41:29 -0500

    commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
     <!--include php script to connect to database-->
     <?php
-        include 'connectdb.php';
+        include 'connectdbb.php';
     ?>
     <div class="container-fluid">
         <div class="page-header">
