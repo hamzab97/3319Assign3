f4172805df84b30641cb85645fc3a07588857e38
hamzab97 <hbana3@uwo.ca>
2018-11-24 13:57:39 -0500

    commit

--- getProduct.php
+++ getProduct.php
@@ -1,12 +1,13 @@
 <?php
-    $query = "SELECT * FROM Product ORDER BY Product.Description"; //get product data and order it by the product description
+    $query = "SELECT Product.Description FROM Product JOIN BoughtBy ON Product.ProductID = BoughtBy.ProductID JOIN Customer ON Customer.CustomerID = BoughtBy.CustomerID ORDER BY Product.Description"; //get product data and order it by the product description
     $result = mysqli_query($connection,$query);
     if (!$result) {
         die("databases query failed.");
     }
     while ($row = mysqli_fetch_assoc($result)) {
-         var_dump($row);
+         echo "<div class=tab-pane id=$row[FirstName] role=tabpanel>";
          echo $row;
+         echo "</div>";
     }
     mysqli_free_result($result);
 ?>
