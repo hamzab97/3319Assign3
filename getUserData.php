5e12728d1171833384dfc3c553667c8ed42211db
hamzab97 <hbana3@uwo.ca>
2018-11-24 17:05:28 -0500

    commit

--- getUserData.php
+++ getUserData.php
@@ -7,7 +7,7 @@
     }
     echo '<div class="list-group" id="myList" role="tablist">'
     while ($row = mysqli_fetch_assoc($result)) {
-            echo '<input type="radio" name="customername" value"';
+            echo '<input type="radio" name="customername" value="';
             echo $row["FirstName"];
             echo '">'. $row["FirstName"]. " " .$row["LastName"];
     //        var_dump($row);
