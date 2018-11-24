a4862978d62c308ad0df31d13cd4c6e618aff433
hamzab97 <hbana3@uwo.ca>
2018-11-24 17:41:17 -0500

    commit

--- getProduct.php
+++ getProduct.php
@@ -1,15 +1,15 @@
 <?php
     $whichCustomer = $_POST["customers"];
     echo $whichCustomer;
-    $query = "SELECT Product.Description, Customer.FirstName FROM Product, BoughtBy, Customer"; //get product data and order it by the product description
-    $result = mysqli_query($connection,$query);
-    if (!$result) {
-        die("databases query failed.");
-    }
-    while ($row = mysqli_fetch_assoc($result)) {
-         echo '<div class="tab-pane" id="$row["Customer.FirstName"]" role="tabpanel">';
-         echo $row;
-         echo '</div>';
-    }
-    mysqli_free_result($result);
+//    $query = "SELECT Product.Description, Customer.FirstName FROM Product, BoughtBy, Customer"; //get product data and order it by the product description
+//    $result = mysqli_query($connection,$query);
+//    if (!$result) {
+//        die("databases query failed.");
+//    }
+//    while ($row = mysqli_fetch_assoc($result)) {
+//         echo '<div class="tab-pane" id="$row["Customer.FirstName"]" role="tabpanel">';
+//         echo $row;
+//         echo '</div>';
+//    }
+//    mysqli_free_result($result);
 ?>
