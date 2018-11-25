86e77c5626a9384917f2ed66ae21735f95b362b7
hamzab97 <hbana3@uwo.ca>
2018-11-25 14:15:14 -0500

    commit

---
+++ addNewUser.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add new user</title>
+</head>
+    <body>
+
+        <?php
+            include 'connectdb.php';
+        ?>
+
+        <?php
+            echo "<h1>adding new customer ";
+            echo $_POST["customerID"];
+            echo "</h1>";
+            $customerID = $_POST["customerID"]; //customer ID of the customer picked
+            $firstName= $_POST["firstName"];
+            $lastName= $_POST["lastName"];
+            $phoneNumber= $_POST["phoneNumber"];
+            $city= $_POST["city"];
+            $agentID= $_POST["agentID"];
+    // check first if customer id already in db
+            $checkCustomerExists = "SHOW INDEX FROM Customer WHERE CustomerID = '$customerID'";
+            $checkAgentExists = "SHOW INDEX FROM Agent WHERE AgentID = '$agentID'";
+            if ($result = mysqli_query($connection, $checkCustomerExists)){
+                //customer already exists in the db
+                echo '<h1>Customer '. $customerID. ' '. $firstName. ' '. $lastName. ' already exists in the database</h1>';
+            }
+            $result = mysqli_query($connection, $checkAgentExists);
+            if (!$result){
+                //agent does not exists
+                echo '<h1>agent does not exist with that agent ID</h1>';
+            }
+            else{
+                $query = "INSERT INTO Customer VALUES ('$customerID', '$firstName', '$lastName', '$city', '$phoneNumber', '$agentID')";
+                $result = mysqli_query($connection,$query);
+                if (!$result) {
+                    die("databases query failed.");
+                }
+                while ($row = mysqli_fetch_assoc($result)) {
+                     echo '<p>inserted customer into db</p>';
+                }
+                mysqli_free_result($result);
+            }
+        ?>
+
+        <form action="index.php" method="post">
+             <input type="submit" value="Go back to homepage">
+        </form>
+
+    </body>
+</html>
--- getProduct.php
+++ getProduct.php
@@ -4,36 +4,36 @@
     <meta charset="UTF-8">
     <title>Products</title>
 </head>
-<body>
+    <body>
 
-    <?php
-            include 'connectdb.php';
-    ?>
+        <?php
+                include 'connectdb.php';
+        ?>
 
-    <?php
-        echo "<h1>Products purchased by customer ";
-        echo $_POST["customername"];
-        echo "</h1>";
-        $whichCustomer = $_POST["customername"]; //customer ID of the customer picked
-//        echo '<h1>hello';
-//        echo $whichCustomer;
-//        echo '</h1>';
-        $query = 'SELECT Product.Description FROM Product JOIN BoughtBy ON Product.ProductID = BoughtBy.ProductID AND BoughtBy.CustomerID ="' . $whichCustomer . '"';
-        $result = mysqli_query($connection,$query);
-        if (!$result) {
-            die("databases query failed.");
-        }
-        while ($row = mysqli_fetch_assoc($result)) {
-             echo '<p>';
-             echo $row;
-             echo '</p>';
-        }
-        mysqli_free_result($result);
-    ?>
+        <?php
+            echo "<h1>Products purchased by customer ";
+            echo $_POST["customername"];
+            echo "</h1>";
+            $whichCustomer = $_POST["customername"]; //customer ID of the customer picked
+    //        echo '<h1>hello';
+    //        echo $whichCustomer;
+    //        echo '</h1>';
+            $query = 'SELECT Product.Description FROM Product JOIN BoughtBy ON Product.ProductID = BoughtBy.ProductID AND BoughtBy.CustomerID ="' . $whichCustomer . '"';
+            $result = mysqli_query($connection,$query);
+            if (!$result) {
+                die("databases query failed.");
+            }
+            while ($row = mysqli_fetch_assoc($result)) {
+                 echo '<p>';
+                 echo $row;
+                 echo '</p>';
+            }
+            mysqli_free_result($result);
+        ?>
 
-    <form action="index.php" method="post">
-         <input type="submit" value="Go back to homepage">
-    </form>
+        <form action="index.php" method="post">
+             <input type="submit" value="Go back to homepage">
+        </form>
 
-</body>
+    </body>
 </html>
