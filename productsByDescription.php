0eb48cb1396f8637700e704b7e58ef14f2c26469
hamzab97 <hbana3@uwo.ca>
2018-11-26 18:49:51 -0500

    commit

--- productsByDescription.php
+++ productsByDescription.php
@@ -24,7 +24,7 @@
             }
             while ($row = mysqli_fetch_assoc($result)) {
                  echo '<p>';
-                 echo $row["Product.Description"]. " ". $row["Product.CostPerItem"];
+                 echo $row["Product.Description"]. " ". $row[1];
                  echo '</p>';
             }
             mysqli_free_result($result);
