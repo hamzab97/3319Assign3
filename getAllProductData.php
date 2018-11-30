5c473787045c3d3b47e1126467d95f55a8259d76
hamzab97 <hbana3@uwo.ca>
2018-11-30 14:28:25 -0500

    commit

--- getAllProductData.php
+++ getAllProductData.php
@@ -16,16 +16,20 @@
             echo $productname;
             echo '</h1>';
             //select * from BoughtBy where BoughtBy.ProductID = (select Product.ProductID from Product where Product.Description = "Socks");
-            $query = 'SELECT * FROM BoughtBy WHERE BoughtBy.ProductID = (SELECT Product.ProductID FROM Product WHERE Product.Description="' . $productname . '")';
+            $query = 'SELECT SUM(Quantity) FROM BoughtBy WHERE BoughtBy.ProductID = (SELECT Product.ProductID FROM Product WHERE Product.Description="' . $productname . '")';
             $result = mysqli_query($connection,$query);
             if (!$result) {
                 die("databases query failed.");
             }
-            while ($row = mysqli_fetch_assoc($result)) {
-                 echo '<p> hoi : ';
-                 echo $row["Quantity"];
-                 echo '</p>';
-            }
+            $row = mysqli_fetch_assoc($result);
+            echo '<p>total quantity of products purchased ';
+            echo $row["SUM(Quantity)"];
+            echo '</p>';
+//            while ($row = mysqli_fetch_assoc($result)) {
+//                 echo '<p> hoi : ';
+//                 echo $row["SUM(Quantity)"];
+//                 echo '</p>';
+//            }
             mysqli_free_result($result);
         ?>
 
